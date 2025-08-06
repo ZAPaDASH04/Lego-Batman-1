1e8849812c266183501a4bf10f25e42e607ab107
jrad <215300829+jradcode23@users.noreply.github.com>
2025-08-05 19:48:42 -0700

    Start setting up regions & init.py

---
+++ worlds/lego_batman_1/Regions.py
@@ -0,0 +1,55 @@
+import typing
+
+from BaseClasses import MultiWorld, Region, Entrance, Location
+from .Options import LB1Options
+from .Locations import LB1Location, location_table, minikit_location_table
+
+
+class LB1Region(Region):
+    subregions: typing.List[Region] = []
+
+
+lb1_regions = [
+    "Menu",
+    "Batcave",
+    "Arkham Asylum",
+    "Status Screen"
+    "You can Bank on Batman",
+    # "An Icy Reception",
+    # "Two-Face Chase",
+    # "A Poisonous Appointment",
+    # "The Face-Off",
+    # "There She Goes Again",
+    # "Batboat Battle"
+]
+
+
+def create_regions(world: MultiWorld, options: LB1Options, player: int):
+    menu = Region("Menu", player, world)
+    batcave = create_region("Batcave", player, world)
+    arkham_asylum = create_region("Arkham Asylum", player, world)
+    status_screen = create_region("Status Screen", player, world)
+    you_can_bank_on_batman = create_region("You Can Bank on Batman", player, world)
+
+    # connect_regions(world, player, menu, batcave)
+
+
+    for name in minikit_location_table:
+        if name.startswith("You can Bank on Batman"):
+            create_location(you_can_bank_on_batman, name)
+
+
+def connect_regions(world: MultiWorld, player: int, source: str, target: str, rule=None) -> Entrance:
+    source_region = world.get_region(source, player)
+    target_region = world.get_region(target, player)
+    return source_region.connect(target_region, rule=rule)
+
+
+def create_region(name: str, player: int, world: MultiWorld) -> LB1Region:
+    region = LB1Region(name, player, world)
+    world.regions.append(region)
+    return region
+
+
+def create_location(reg: Region, *location: str):
+    reg.locations += [LB1Location(reg.player, loc_name, location_table[loc_name], reg) for loc_name in location]
--- worlds/lego_batman_1/__init__.py
+++ worlds/lego_batman_1/__init__.py
@@ -4,12 +4,12 @@ from BaseClasses import Item, ItemClassification, Tutorial, Region, MultiWorld
 from ..AutoWorld import World, WebWorld
 
 from .Items import LB1Item, item_table
-from .Locations import LB1Location
+from .Locations import location_table, LB1Location
 from .Options import LB1Options
 from .Regions import create_regions, connect_regions, LB1Region
-from .Rules import set_rules
+# from .Rules import set_rules
 
-class LegoBatman1(WebWorld):
+class LB1(WebWorld):
     theme = "ocean"
     tutorials = [Tutorial(
         "Multiworld Setup Guide",
@@ -21,44 +21,20 @@ class LegoBatman1(WebWorld):
     )]
 
 
-class LegoBatman1World(World):
+class LB1World(World):
     """
      When all the villains in Arkham Asylum team up and break loose, only the dynamic duo is bold enough to take them on to save Gotham City. 
      The fun of LEGO, the drama of Batman and the uniqueness of the combination makes for a comical and exciting adventure in LEGO Batman: The Videogame. 
     """ #Lazy Import
     game = "Lego Batman: The Video Game"
-    options_dataclass = LegoBatman1Options
-    options: LegoBatman1Options
-    topology_present = True
+    options_dataclass = LB1Options
+    options: LB1Options
+    topology_present = False
 
-    item_name_to_id = {name: data.code for name, data in item_table.items()}
-    location_name_to_id = {name: data.code for name, data in location_table.items()}
-
-    
-    item_name_groups = {
-        # clayface mrfreeze poisonivy twoface riddler
-        # bane catwoman+catwoman2 killercroc manbat penguin
-        # harleyquinn scarecrow killermoth madhatter joker+jokerhawaiian
-        "batman": {"batman","batgirl"},
-        "robin": {"robin","nightwing"},
-        "batarang": {"batman","robin","batgirl","nightwing"},
-        "grapple": {"batman","robin","batgirl","nightwing"},
-        # "gun": {},
-        "toxic": {"biohazardscientist","mrfreeze","poisonivy","twoface","bane","killercroc","joker","jokerhawaiian"},
-        "strong": {"clayface","mrfreeze","bane","killercroc","manbat"},
-        "doublejump": {"clayface","poisonivy", "catwoman", "catwoman2", "madhatter"},
-        "hypnosis": {"riddler","scarecrow","madhatter"}, # hypnosis + security_access "NoDoor" (questionmarks)
-        "femininewiles": {"poisonivy","catwoman","harleyquinn"}, # security_access "LoveHearts"
-        "tightrope": {"robin","brucewayne","alfred","batgirl","nightwing","commissionergordon","policeman_2","fish_monger","military_policeman","securityguard_1","swatteam"},
-        "explosive": {"suit_demolition","penguin"},
-        "glide": {"suit_glide", "manbat", "killermoth", "penguin"},
-        "sink": {"suit_scuba", "killercroc"},
-        # unique abilities
-        "mrfreeze": {"mrfreeze"},
-        "poisonivy": {"poisonivy"},
-    }
+    item_name_to_id = item_table
+    location_name_to_id = location_table
 
     data_version = 1
     required_client_version = (0, 4, 4)
-    web = LegoBatman1Web()
+    # web = LB1Web()
    
