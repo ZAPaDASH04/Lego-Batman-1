43eccf9f3e543b9d82b7c188a02d89df0a8d1600
jrad <215300829+jradcode23@users.noreply.github.com>
2025-08-08 21:23:16 -0700

    Adding in the rest of the hero side minikits

--- worlds/lego_batman_1/Regions.py
+++ worlds/lego_batman_1/Regions.py
@@ -18,8 +18,16 @@ lb1_regions = [
     "Two-Face Chase",
     "A Poisonous Appointment",
     "The Face-Off",
-    # "There She Goes Again",
-    # "Batboat Battle"
+    "There She Goes Again",
+    "Batboat Battle",
+    "Under the City",
+    "Zoo's Company",
+    "Penguin's Lair",
+    "Joker's Home Turf",
+    "Little Fun at Big Top",
+    "Flight of the Bat",
+    "In the Dark Night",
+    "To the Top of the Tower"
 ]
 
 
@@ -48,6 +56,36 @@ def create_regions(world: MultiWorld, options: LB1Options, player: int):
     the_face_off = create_region("The Face-Off", player, world)
     world.regions.append(the_face_off)
 
+    there_she_goes_again = create_region("There She Goes Again", player, world)
+    world.regions.append(there_she_goes_again)
+
+    batboat_battle = create_region("Batboat Battle", player, world)
+    world.regions.append(batboat_battle)
+
+    under_the_city = create_region("Under the City", player, world)
+    world.regions.append(under_the_city)
+
+    zoos_company = create_region("Zoo's Company", player, world)
+    world.regions.append(zoos_company)
+
+    penguins_lair = create_region("Penguin's Lair", player, world)
+    world.regions.append(penguins_lair)
+
+    jokers_home_turf = create_region("Joker's Home Turf", player, world)
+    world.regions.append(jokers_home_turf)
+
+    little_fun_at_big_top = create_region("Little Fun at Big Top", player, world)
+    world.regions.append(little_fun_at_big_top)
+
+    flight_of_the_bat = create_region("Flight of the Bat", player, world)
+    world.regions.append(flight_of_the_bat)
+
+    in_the_dark_night = create_region("In the Dark Night", player, world)
+    world.regions.append(in_the_dark_night)
+
+    to_the_top_of_the_tower = create_region("To the Top of the Tower", player, world)
+    world.regions.append(to_the_top_of_the_tower)
+
     connect_regions(world, player, "Menu", "Batcave")
     connect_regions(world, player, "Batcave", "Arkham Asylum")
     connect_regions(world, player, "Batcave", "You can Bank on Batman")
@@ -55,7 +93,16 @@ def create_regions(world: MultiWorld, options: LB1Options, player: int):
     connect_regions(world, player, "Batcave", "Two-Face Chase")
     connect_regions(world, player, "Batcave", "A Poisonous Appointment")
     connect_regions(world, player, "Batcave", "The Face-Off")
-
+    connect_regions(world, player, "Batcave", "There She Goes Again")
+    connect_regions(world, player, "Batcave", "Batboat Battle")
+    connect_regions(world, player, "Batcave", "Under the City")
+    connect_regions(world, player, "Batcave", "Zoo's Company")
+    connect_regions(world, player, "Batcave", "Penguin's Lair")
+    connect_regions(world, player, "Batcave", "Joker's Home Turf")
+    connect_regions(world, player, "Batcave", "Little Fun at Big Top")
+    connect_regions(world, player, "Batcave", "Flight of the Bat")
+    connect_regions(world, player, "Batcave", "In the Dark Night")
+    connect_regions(world, player, "Batcave", "To the Top of the Tower")
 
     for name in location_table:
         if name.startswith("You can Bank on Batman"):
@@ -68,7 +115,26 @@ def create_regions(world: MultiWorld, options: LB1Options, player: int):
             create_location(a_poisonous_appointment, name)
         elif name.startswith("The Face-Off"):
             create_location(the_face_off, name)
-
+        elif name.startswith("There She Goes Again"):
+            create_location(there_she_goes_again, name)
+        elif name.startswith("Batboat Battle"):
+            create_location(batboat_battle, name)
+        elif name.startswith("Under the City"):
+            create_location(under_the_city, name)
+        elif name.startswith("Zoo's Company"):
+            create_location(zoos_company, name)
+        elif name.startswith("Penguin's Lair"):
+            create_location(penguins_lair, name)
+        elif name.startswith("Joker's Home Turf"):
+            create_location(jokers_home_turf, name)
+        elif name.startswith("Little Fun at Big Top"):
+            create_location(little_fun_at_big_top, name)
+        elif name.startswith("Flight of the Bat"):
+            create_location(flight_of_the_bat, name)
+        elif name.startswith("In the Dark Night"):
+            create_location(in_the_dark_night, name)
+        elif name.startswith("To the Top of the Tower"):
+            create_location(to_the_top_of_the_tower, name)
 
 def connect_regions(world: MultiWorld, player: int, source: str, target: str, rule=None) -> Entrance:
     source_region = world.get_region(source, player)
